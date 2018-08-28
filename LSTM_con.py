142fe00329fbe201001c046d76fb1c3f9684e26e
GLASMAN Conor <c.glasman@bravosolution.com>
2018-08-28 18:59:54 +0100

    Adding note for why LSTMs are being learnt

---
+++ LSTM_con.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 23 09:59:05 2018
+
+@author: ConorGL
+
+This is part 2 of attempting to build the Florida Man model. I will create an 
+LSTM in order to predict words in a coherent sentence structure.
+"""
+
